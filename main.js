f703463c6e28871005eae2fa5bb78281bd56e569
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2023-07-29 20:53:20 +0700

    add express framework, pkg file, dockerfile

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-var http = require('http');
-var url = require('url');
+const express = require('express');
 
-http.createServer(function (req, res) {
-    var retText = ''
-    res.writeHead(200, {'Content-Type':'text/html'})
-    retText += 'URL in browser is: "' + req.url + '"<br/>'
+const POST = 8080;
+const HOST = '0.0.0.0';
 
-    var params = url.parse(req.url, true).query;
-    retText += 'receiving params:<br/>'
+const app = express();
 
-    for (const [key, value] of Object.entries(params)) {
-        retText += 'key: '+key + ' ,value: '+value + '<br/>'
-    }
+// handle HTTP request /
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 
-    res.write(retText)
-    res.end()
-}).listen(1234)
+app.listen(POST, HOST, () => {
+    console.log('starting application at %s:%d', HOST, POST)
+})
